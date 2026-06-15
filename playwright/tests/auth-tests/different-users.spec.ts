43ba14db6ca4491b0ebe67e0fc7224d1e3d7fb77
Floris Heins <floris.heins@rws.nl>
2026-06-15 17:31:07 +0200

    added stuff

---
+++ playwright/tests/auth-tests/different-users.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test'
+
+const baseUrl = process.env['BASE_URL'] || ''
+
+test('price of first item', async ({ page }) => {
+  await page.goto(`${baseUrl}/inventory.html`)
+
+  // expect the first item to be priced at 29.99
+  await expect(
+    page.locator('[data-test="inventory-item-description"]').first()
+    .locator('[data-test="inventory-item-price"]'))
+    .toContainText('29.99')
+})
+
+test.describe('visual user tests', () => {
+  test.use({
+    storageState: 'tests/.auth/visual-user.json'
+  })
+  test('price of first item', async ({ page }) => {
+    await page.goto(`${baseUrl}/inventory.html`)
+
+    // expect the first item to be priced at 29.99
+    await expect(
+      page.locator('[data-test="inventory-item-description"]').first()
+      .locator('[data-test="inventory-item-price"]'))
+      .not.toContainText('29.99')
+  })
+})
+
+// TODO: run these tests
---
+++ playwright/tests/auth.setup.ts
@@ -0,0 +1,37 @@
+import { test as setup, expect } from '@playwright/test'
+import path from 'path'
+
+const standardUserAuthFile = path.join(__dirname, '.auth/standard-user.json')
+
+const visualUserAuthFile = path.join(__dirname, '.auth/visual-user.json')
+
+const password = process.env['PASSWORD'] || ''
+const baseUrl = process.env['BASE_URL'] || ''
+const standard_user = 'standard_user'
+const visual_user = 'visual_user'
+
+setup('authenticate standard user', async ({ page }) => {
+  await page.goto(baseUrl)
+
+  await page.locator('[data-test="username"]').fill(standard_user)
+  await page.locator('[data-test="password"]').fill(password)
+  await page.locator('[data-test="login-button"]').click()
+
+  await page.waitForURL(`${baseUrl}/inventory.html`, { timeout: 5000 })
+  await expect(page.getByRole('button', { name: 'Open Menu' })).toBeVisible()
+
+  await page.context().storageState({ path: standardUserAuthFile })
+})
+
+setup('authenticate visual user', async ({ page }) => {
+  await page.goto(baseUrl)
+
+  await page.locator('[data-test="username"]').fill(visual_user)
+  await page.locator('[data-test="password"]').fill(password)
+  await page.locator('[data-test="login-button"]').click()
+
+  await page.waitForURL(`${baseUrl}/inventory.html`, { timeout: 5000 })
+  await expect(page.getByRole('button', { name: 'Open Menu' })).toBeVisible()
+
+  await page.context().storageState({ path: visualUserAuthFile })
+})
